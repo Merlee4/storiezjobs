a05270a1b267d31fca48775be1e86aa52dea96f4
Mwelwa Nkuta <mwelwankuta@mail.com>
2021-04-24 10:07:39 -0700

    suicide

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,35 +1,44 @@
-import { BriefcaseIcon, ClockIcon, LocationMarkerIcon } from '@heroicons/react/outline'
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import {
+  BriefcaseIcon,
+  ClockIcon,
+  LocationMarkerIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-function JobCard({image, title, name, duration, location, id }) {
-
-    const router = useHistory()
-    return (
-        <div className="flex p-2 cursor-pointer hover:shadow-lg rounded transition duration-200 items-center" onClick={() => router.push(`/jobs/${id}`)} >
-            <div className="mr-2">
-                <img src={image} alt="job" className="h-14 w-14 object-cover rounded " />
-            </div>
-            <div className="justify-start ">
-                <p className="font-medium">{title}</p>
-                <div className=" items-left text-sm text-gray-700 flex flex-col">
-                    <div className="flex items-center gap-2">
-                        <BriefcaseIcon className="h-4 ml-1 text-blue-800" />
-                        <p>{name}</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <LocationMarkerIcon className="h-4 ml-1 text-red-600" />
-                        <p>{location}</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <ClockIcon className="h-4 ml-1 text-pink-800" />
-                        <p>{duration}</p>
-                    </div>
-                </div>
-            </div>
+function JobCard({ image, title, name, duration, location, id }) {
+  const router = useHistory();
+  return (
+    <div
+      className="flex p-2 cursor-pointer hover:shadow-lg rounded transition duration-200 items-center"
+      onClick={() => router.push(`/jobs/${id}`)}
+    >
+      <div className="mr-2">
+        <img
+          src={image}
+          alt="job"
+          className="h-14 w-14 object-cover rounded "
+        />
+      </div>
+      <div className="justify-start ">
+        <p className="font-medium">{title}</p>
+        <div className=" items-left text-sm text-gray-700 flex flex-col">
+          <div className="flex items-center gap-2">
+            <BriefcaseIcon className="h-4 ml-1 text-blue-800" />
+            <p>{name}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <LocationMarkerIcon className="h-4 ml-1 text-red-600" />
+            <p>{location}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <ClockIcon className="h-4 ml-1 text-pink-800" />
+            <p>{duration}</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default JobCard
+export default JobCard;
--- src/components/ProfileMainContent.js
+++ src/components/ProfileMainContent.js
@@ -1,85 +1,98 @@
-import { BriefcaseIcon, LocationMarkerIcon, UserGroupIcon } from '@heroicons/react/outline'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import JobsList from './JobsList'
-import PrompModal from './PrompModal'
+import { LocationMarkerIcon, UserGroupIcon } from "@heroicons/react/outline";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import JobsList from "./JobsList";
+import PrompModal from "./PrompModal";
 
 function ProfileMainContent({ jobdata }) {
-    const [Apply, setApply] = useState(false)
-    const [myJobs, setMyJobs] = useState([])
-    const job = jobdata
-    console.log(job)
-    const setIsOpen = () => {
-        setApply(false)
-    }
+  const [Apply, setApply] = useState(false);
+  const [myJobs, setMyJobs] = useState([]);
+  const job = jobdata;
+  console.log(job);
+  const setIsOpen = () => {
+    setApply(false);
+  };
 
-    useEffect(() => {
-        axios.get(`http://localhost:9000/job/${jobdata._id}`)
-        .then(res => {
-            setMyJobs(res.data)
-        })
-    },[])
-    return (
-        <div>
-            <div className="">
-                <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_200_200/0/1519856215226?e=1626912000&v=beta&t=d8TvzdxEh-ZthnCjpGmRIgBwvWTZG-NF230xBZ4iZXU" className="h-28 w-28 object-fill" alt="profile" />
-                <h1 className="text-2xl my-2 font-medium text-gray-800">Google</h1>
-            </div>
-            <div>
-                
-                <div className="flex items-center gap-2">
-                    <LocationMarkerIcon className="h-4 gap-2" />
-                    <p>{jobdata.location}</p>
-                </div>
-            </div>
+  useEffect(() => {
+    axios.get(`http://localhost:9000/job/${jobdata._id}`).then((res) => {
+      setMyJobs(res.data);
+    });
+  });
+  return (
+    <div>
+      <div className="">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_200_200/0/1519856215226?e=1626912000&v=beta&t=d8TvzdxEh-ZthnCjpGmRIgBwvWTZG-NF230xBZ4iZXU"
+          className="h-28 w-28 object-fill"
+          alt="profile"
+        />
+        <h1 className="text-2xl my-2 font-medium text-gray-800">Google</h1>
+      </div>
+      <div>
+        <div className="flex items-center gap-2">
+          <LocationMarkerIcon className="h-4 gap-2" />
+          <p>{jobdata.location}</p>
+        </div>
+      </div>
 
-            <div>
-                <div className="flex gap-2">
-                    <button onClick={() => setApply(true)} className="py-2 px-4 bg-blue-600 rounded font-medium text-white mt-2">Apply Now</button>
+      <div>
+        <div className="flex gap-2">
+          <button
+            onClick={() => setApply(true)}
+            className="py-2 px-4 bg-blue-600 rounded font-medium text-white mt-2"
+          >
+            Apply Now
+          </button>
 
-                    <div className="flex items-center gap-2">
-                        <UserGroupIcon className="h-4" />
-                        <p>Employees</p>
-                        <p className="font-medium">{job.employees}</p>
-                    </div>
-                </div>
-                <p className="text-sm text-red-700">{job.available}</p>
-            </div>
-            <div className="rounded-lg border py-4 px-8 bg-white text-gray-700 mt-4">
-                <h1 className="text-2xl my-4 font-medium">About the Role</h1>
-                <p className="">{job.about}</p>
-                
-                <h1 className="text-xl my-4 font-medium">Requirements</h1>
-                <ul className="list-disc ml-8">
-                    {job.requirements.map(req => 
-                        <li>{req}</li>
-                    )}
-                </ul>
-                <p>{job.requirementsText}</p>
+          <div className="flex items-center gap-2">
+            <UserGroupIcon className="h-4" />
+            <p>Employees</p>
+            <p className="font-medium">{job.employees}</p>
+          </div>
+        </div>
+        <p className="text-sm text-red-700">{job.available}</p>
+      </div>
+      <div className="rounded-lg border py-4 px-8 bg-white text-gray-700 mt-4">
+        <h1 className="text-2xl my-4 font-medium">About the Role</h1>
+        <p className="">{job.about}</p>
 
-                <p className="py-4">Check out our career opportunities</p>
-                <div className="mx-auto">
-                    <JobsList jobs={myJobs}/>
-                </div>
-                <div className="flex justify-between gap-4">
-                    <div className="rounded-lg border p-4 my-4 w-full">
-                        <p className="text-sm">Contact info</p>
-                        <Link className="text-blue-700 hover:underline font-medium">google.com</Link>
-                        <p className="text-sm my-2">Location </p>
-                        <button className="p-2 rounded bg-gray-200">Show on map</button>
-                    </div>
-                    <div className="rounded-lg border p-4 my-4 w-full">
-                        <p className="text-sm">Email us</p>
-                        <Link className="text-blue-700 hover:underline font-medium">jobs@google.com</Link>
-                        <p className="text-sm">Phone </p>
-                        <Link className="text-blue-700 hover:underline font-medium">+260 964487617</Link>
-                    </div>
-                </div>
-            </div>
-            <PrompModal isOpen={Apply} setIsOpen={setIsOpen} />
-        </div >
-    )
+        <h1 className="text-xl my-4 font-medium">Requirements</h1>
+        <ul className="list-disc ml-8">
+          {job.requirements.map((req) => (
+            <li>{req}</li>
+          ))}
+        </ul>
+        <p>{job.requirementsText}</p>
+
+        <p className="py-4">Check out our career opportunities</p>
+        <div className="mx-auto">
+          <JobsList jobs={myJobs} />
+        </div>
+        <div className="flex justify-between gap-4">
+          <div className="rounded-lg border p-4 my-4 w-full">
+            <p className="text-sm">Contact info</p>
+            <Link className="text-blue-700 hover:underline font-medium">
+              google.com
+            </Link>
+            <p className="text-sm my-2">Location </p>
+            <button className="p-2 rounded bg-gray-200">Show on map</button>
+          </div>
+          <div className="rounded-lg border p-4 my-4 w-full">
+            <p className="text-sm">Email us</p>
+            <Link className="text-blue-700 hover:underline font-medium">
+              jobs@google.com
+            </Link>
+            <p className="text-sm">Phone </p>
+            <Link className="text-blue-700 hover:underline font-medium">
+              +260 964487617
+            </Link>
+          </div>
+        </div>
+      </div>
+      <PrompModal isOpen={Apply} setIsOpen={setIsOpen} />
+    </div>
+  );
 }
 
-export default ProfileMainContent
+export default ProfileMainContent;
